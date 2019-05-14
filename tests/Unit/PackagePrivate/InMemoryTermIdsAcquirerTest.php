3acc58e88eb1065f2eb305a4feb2a350d49ae2d2
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-14 13:25:51 +0200

    Make InMemoryTermIdsAcquirer implement TermCleaner
    The TermIdsAcquirer and the TermCleaner clearly have to manipulate the
    same state somehow. For the MediaWiki implementation, this happens via
    the database; for the in-memory implementation, I’m going the easy route
    and making one class implement both interfaces (rather than, for
    example, having a third class with the actual state – the $terms array –
    and two classes accessing that and implementing the acquirer and cleaner
    parts). Some of the tests could be reused for other cleaners as well in
    the future, if we wanted.

--- tests/Unit/PackagePrivate/InMemoryTermIdsAcquirerTest.php
+++ tests/Unit/PackagePrivate/InMemoryTermIdsAcquirerTest.php
@@ -4,6 +4,7 @@ namespace Wikibase\TermStore\MediaWiki\Tests\Unit\PackagePrivate;
 
 use PHPUnit\Framework\TestCase;
 use Wikibase\TermStore\MediaWiki\PackagePrivate\InMemoryTermIdsAcquirer;
+use Wikimedia\TestingAccessWrapper;
 
 class InMemeoryTermIdsAcquirerTest extends TestCase {
 
@@ -89,6 +90,85 @@ class InMemeoryTermIdsAcquirerTest extends TestCase {
 			count( $ids ),
 			count( array_unique( $ids ) )
 		);
+	}
+
+	public function testCleanTerms_doesNotReuseIds() {
+		$termIdsAcquirer = new InMemoryTermIdsAcquirer();
+
+		$ids1 = $termIdsAcquirer->acquireTermIds( [
+			'label' => [
+				'en' => 'the label',
+				'de' => 'die Bezeichnung',
+			],
+			'alias' => [
+				'en' => [ 'alias', 'another' ],
+			],
+		] );
+		$ids2 = $termIdsAcquirer->acquireTermIds( [
+			'label' => [ 'en' => 'the label' ],
+			'description' => [ 'en' => 'the description' ],
+		] );
+
+		$termIdsAcquirer->cleanTerms( array_merge( $ids1, $ids2 ) );
+
+		$ids3 = $termIdsAcquirer->acquireTermIds( [
+			'label' => [ 'en' => 'the label' ],
+			'description' => [ 'en' => 'the description' ],
+		] );
+		$this->assertGreaterThan( max( max( $ids1 ), max( $ids2 ) ), min( $ids3 ) );
+	}
+
+	public function testCleanTerms_completelyCleansArray() {
+		$termIdsAcquirer = new InMemoryTermIdsAcquirer();
+
+		$ids1 = $termIdsAcquirer->acquireTermIds( [
+			'label' => [
+				'en' => 'the label',
+				'de' => 'die Bezeichnung',
+			],
+			'alias' => [
+				'en' => [ 'alias', 'another' ],
+			],
+		] );
+		$ids2 = $termIdsAcquirer->acquireTermIds( [
+			'label' => [ 'en' => 'the label' ],
+			'description' => [ 'en' => 'the description' ],
+		] );
+
+		$termIdsAcquirer->cleanTerms( array_merge( $ids1, $ids2 ) );
+
+		$this->assertEmpty( TestingAccessWrapper::newFromObject( $termIdsAcquirer )->terms );
+	}
+
+	public function testCleanTerms_keepsOtherIds() {
+		$termIdsAcquirer = new InMemoryTermIdsAcquirer();
+
+		$ids1 = $termIdsAcquirer->acquireTermIds( [
+			'label' => [
+				'en' => 'the label',
+				'de' => 'die Bezeichnung',
+			],
+			'alias' => [
+				'en' => [ 'alias', 'another' ],
+			],
+		] );
+		$ids2 = $termIdsAcquirer->acquireTermIds( [
+			'label' => [ 'en' => 'the label' ],
+			'description' => [ 'en' => 'the description' ],
+		] );
+
+		// Clean up terms related to the first set of IDs,
+		// while keeping the second set of IDs intact.
+		// (Knowing that the intersection of $ids1 and $ids2 should not be cleaned
+		// is the caller’s responsibility, not the cleaner’s; we do it via array_diff.)
+		$idsToClean = array_diff( $ids1, $ids2 );
+		$termIdsAcquirer->cleanTerms( $idsToClean );
+
+		$ids3 = $termIdsAcquirer->acquireTermIds( [
+			'label' => [ 'en' => 'the label' ],
+			'description' => [ 'en' => 'the description' ],
+		] );
+		$this->assertSame( $ids2, $ids3 );
 	}
 
 }
